c301c76032b3bb44940022255943a98178e2b5a2
alexander-berezhnoy <berrezhnoy@gmail.com>
2019-09-13 18:55:46 +0300

    Create useFetch custom hook for data fetching and use it in App.js (first version, can be updated later)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import useFetch from './components/useFetch';
 
-const App = () => <h1>Simple Blog</h1>;
+const App = () => {
+    const res = useFetch('https://simple-blog-api.crew.red/comments');
+    if (!res.response) {
+        return <div>Loading</div>;
+    }
+    return (
+        <div className="App">
+            <h1>List of Posts</h1>
+            <div className="post-container">
+                {res.response.map(post => (
+                    <div key={post.id}>
+                        <h3>{`#${post.id} ${post.title}`}</h3>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default App;
